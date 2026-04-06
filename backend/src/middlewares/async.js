45a23eb1031ff6a0900868670c0bbd85b6b8b553
zachflem <zach@seezed.net>
2026-04-07 09:48:40 +1000

    file cleanup before starting again

--- backend/src/middlewares/async.js
+++
@@ -1,9 +0,0 @@
-/**
- * Async handler to wrap async functions and avoid try-catch blocks in controllers
- * @param {Function} fn - The async function to wrap
- * @returns {Function} - The wrapped function
- */
-const asyncHandler = fn => (req, res, next) =>
-  Promise.resolve(fn(req, res, next)).catch(next);
-
-module.exports = asyncHandler;
